1c635665d75a44eac5e112700ec67723c81c2c0e
caohongjian <caohongjian@bigo.sg>
2021-11-21 01:38:39 +0800

    EXCLUDE_FROM_ALL

--- cmake/module/FindLLVM.cmake
+++ cmake/module/FindLLVM.cmake
@@ -3,4 +3,7 @@ include (FetchContent)
 FetchContent_Declare(llvm
     URL https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.1/llvm-12.0.1.src.tar.xz
 )
-FetchContent_MakeAvailable(llvm)
+if(NOT llvm_POPULATED)
+    FetchContent_Populate(llvm)
+    add_subdirectory(${llvm_SOURCE_DIR} ${LLVM_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
